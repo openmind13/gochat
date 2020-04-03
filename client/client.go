66b7ec7781973a5d660f019866f9213c1be321d0
openmind13 <pirogov27@yahoo.com>
2020-04-03 13:54:10 +0300

    added nicknames and terminal colors

--- client/client.go
+++ client/client.go
@@ -13,9 +13,18 @@ const (
 	serverAddr = ":5050"
 )
 
+// terminal colors
+const (
+	reset  = "\033[0m"
+	red    = "\033[1;31m"
+	yellow = "\033[1;33m"
+	blue   = "\033[0;34m"
+)
+
 var (
-	buffer  = make([]byte, 512)
-	message string
+	buffer   = make([]byte, 512)
+	message  string
+	nickname string
 )
 
 func main() {
@@ -24,13 +33,40 @@ func main() {
 		fmt.Println("Failed to connect to the server\nExit program")
 		os.Exit(0)
 	}
-	defer conn.Close()
 
-	go recieveMessages(conn)
+	setNickname(conn)
+
+	go recieve(conn)
 
 	for {
-		message := inputString()
+		message = inputString()
 		conn.Write([]byte(message))
+	}
+}
+
+func setNickname(conn net.Conn) {
+	var (
+		requestNick string
+		answer      string
+	)
+	for {
+		fmt.Printf("%sEnter your nickname > %s", red, reset)
+		requestNick = inputString()
+		conn.Write([]byte(requestNick))
+
+		length, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("error in setNickname")
+			log.Fatal(err)
+		}
+
+		answer = string(buffer[:length])
+		if answer == "ok" {
+			nickname = requestNick
+			return
+		}
+
+		fmt.Println("Nickname is used or invalid. Try again")
 	}
 }
 
@@ -42,13 +78,21 @@ func inputString() string {
 	return strings.Replace(msg, "\n", "", -1)
 }
 
-func recieveMessages(conn net.Conn) {
+func recieve(conn net.Conn) {
 	for {
 		length, err := conn.Read(buffer)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("%sServer stopped!%s", red, reset)
+			os.Exit(0)
 		}
 		message = string(buffer[:length])
-		fmt.Println(message)
+		log.Printf("| %s%s%s", blue, message, reset)
 	}
 }
+
+// func send(conn net.Conn) {
+// 	for {
+// 		message = inputString()
+// 		conn.Write([]byte(message))
+// 	}
+// }
--- server/server.go
+++ server/server.go
@@ -10,10 +10,16 @@ const (
 	port = ":5050"
 )
 
+// terminal colors
+const (
+	reset  = "\033[0m"
+	red    = "\033[1;31m"
+	yellow = "\033[1;33m"
+	blue   = "\033[0;34m"
+)
+
 var (
-	connections = make(map[net.Conn]bool)
-	buffer      = make([]byte, 512)
-	message     string
+	connections = make(map[net.Conn]string)
 )
 
 func main() {
@@ -22,6 +28,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer server.Close()
+	fmt.Println("Server started!")
 
 	for {
 		conn, err := server.Accept()
@@ -41,20 +48,54 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	connections[conn] = true
-	conn.Write([]byte("Welcome to chat server"))
+	var (
+		buffer   = make([]byte, 512)
+		nickname string
+		message  string
+	)
 
 	for {
 		length, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("client left chat")
+			fmt.Println("error in setting nickname")
+		}
+		nickname = string(buffer[:length])
+		if nicknameIsCorrect(nickname) {
+			conn.Write([]byte("ok"))
+			connections[conn] = nickname
+
+			log.Printf("| %s%s%s %s%s%s", yellow, nickname, reset, red, "joined the chat", reset)
+			//log.Println(nickname + " joined the chat")
+			sendToAll(conn, nickname+" joined the chat")
+
+			break
+		}
+	}
+
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil {
+			log.Printf("| %s%s%s %s%s%s", yellow, nickname, reset, red, "left chat", reset)
 			break
 		}
 		message = string(buffer[:length])
-		fmt.Println(message)
-		sendToAll(conn, message)
+
+		log.Printf("| %s%s%s > %s", yellow, nickname, reset, message)
+		// log.Println(nickname + " > " + message)
+		sendToAll(conn, nickname+" > "+message)
 	}
+
+	sendToAll(conn, nickname+" left the chat")
 	delete(connections, conn)
+}
+
+func nicknameIsCorrect(nick string) bool {
+	for _, value := range connections {
+		if value == nick {
+			return false
+		}
+	}
+	return true
 }
 
 func sendToAll(sender net.Conn, message string) {
